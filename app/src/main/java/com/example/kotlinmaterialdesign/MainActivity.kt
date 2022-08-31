e67ef304a74d42c87c661c1026bd12dd277d49c2
Bahadır Eray <57098047+Bahadireray@users.noreply.github.com>
2022-08-31 23:42:21 +0300

    Activity animation, next homePage

--- app/src/main/java/com/example/kotlinmaterialdesign/MainActivity.kt
+++ app/src/main/java/com/example/kotlinmaterialdesign/MainActivity.kt
@@ -1,22 +1,44 @@
 package com.example.kotlinmaterialdesign
 
+import android.app.ActivityOptions
 import android.content.Intent
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        textView.text="merhaba"
 
+        object : CountDownTimer(1500, 1400) {
+            override fun onTick(millisUntilFinished: Long) {
+                imageViewBig.animate().apply {
+                    duration = 1000
+                    rotationXBy(360f)
+                }.start()
+                imageViewSmall.animate().apply {
+                    duration = 1000
+                    rotationXBy(360f)
+                }.start()
+
+            }
+
+            override fun onFinish() {
+
+                val intent = Intent(this@MainActivity, HomeActivity::class.java)
+                startActivity(intent)
+                overridePendingTransition(R.xml.no_animation,R.xml.slide_down)
+            }
+        }.start()
 
     }
-    fun homeBtn(view: View)
-    {
-        val intent = Intent(this,HomeActivity::class.java)
+
+    fun homeBtn(view: View) {
+        val intent = Intent(this, HomeActivity::class.java)
         startActivity(intent)
     }
 }
