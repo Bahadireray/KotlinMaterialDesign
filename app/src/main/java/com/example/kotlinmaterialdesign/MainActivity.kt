9312d03760dccad537797d3f4b011d11361363d4
Bahadır Eray <57098047+Bahadireray@users.noreply.github.com>
2022-08-31 16:31:20 +0300

    Activity page home

--- app/src/main/java/com/example/kotlinmaterialdesign/MainActivity.kt
+++ app/src/main/java/com/example/kotlinmaterialdesign/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.kotlinmaterialdesign
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_home.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textView.text="merhaba"
+
+        val intent = Intent(this,HomeActivity::class.java)
+        startActivity(intent)
+
+    }
+    fun homeBtn(view: View)
+    {
+        val intent = Intent(this,HomeActivity::class.java)
+        startActivity(intent)
     }
 }
