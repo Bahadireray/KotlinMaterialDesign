3e303d51ffb5ccbc1da1b224e9e61a657d0412e7
Bahadır Eray <57098047+Bahadireray@users.noreply.github.com>
2022-08-31 16:40:39 +0300

    Activity page home

--- app/src/main/java/com/example/kotlinmaterialdesign/HomeActivity.kt
+++ app/src/main/java/com/example/kotlinmaterialdesign/HomeActivity.kt
@@ -2,10 +2,15 @@ package com.example.kotlinmaterialdesign
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+    }
+
+    fun joinButton(view: View) {
+
     }
 }
--- app/src/main/java/com/example/kotlinmaterialdesign/MainActivity.kt
+++ app/src/main/java/com/example/kotlinmaterialdesign/MainActivity.kt
@@ -12,8 +12,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         textView.text="merhaba"
 
-        val intent = Intent(this,HomeActivity::class.java)
-        startActivity(intent)
 
     }
     fun homeBtn(view: View)
