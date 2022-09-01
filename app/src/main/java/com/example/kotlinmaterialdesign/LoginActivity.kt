be7bd5249ace24ae64787844dec870428eb9a684
Bahadır Eray <57098047+Bahadireray@users.noreply.github.com>
2022-09-01 19:50:41 +0300

    login

--- app/src/main/java/com/example/kotlinmaterialdesign/LoginActivity.kt
+++ app/src/main/java/com/example/kotlinmaterialdesign/LoginActivity.kt
@@ -1,9 +1,12 @@
 package com.example.kotlinmaterialdesign
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,6 +25,23 @@ class LoginActivity : AppCompatActivity() {
         finish()
     }
 
+    fun login(view: View) {
+        val sharedPreferences = this.getSharedPreferences(
+            "com.example.kotlinmaterialdesign",
+            Context.MODE_PRIVATE
+        )
+        var etUserName = etUserName.text.toString()
+        var etPassword = etPassword.text.toString()
+
+        val shUserName = sharedPreferences.getString("myUserName", "")
+        val shPassword = sharedPreferences.getString("myPassword", "")
+        if (etUserName == shUserName && etPassword == shPassword) {
+            Toast.makeText(this, "Giriş Başarılı", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, "Bilgileriniz Yanlış", Toast.LENGTH_LONG).show()
+        }
+
+    }
 
     fun signUp(view: View) {
         val intent = Intent(this@LoginActivity, SignUpActivity::class.java)
